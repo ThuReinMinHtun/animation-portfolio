2256537113cbfb2da279ff68dc42aa4081218987
Thu Rein <thureinminhtun347@gmail.com>
2025-01-01 20:21:26 +0900

    merging v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       fontFamily: {
         "space-grotesk": ["Space Grotesk", "sans-serif"],
+        'roboto':['"Roboto"','serif'],
       },
       fontSize: {
         "nm": '20px',
@@ -13,7 +14,11 @@ module.exports = {
       },
       colors: {
         "orange": "#ff6700",
-      }
+        'custom_orange':'#FF6700',
+        'custom_white':'#F3F3F3',
+        'custom_peach' : '#FFE9DA',
+      },
+      
     },
   },
   plugins: [],
